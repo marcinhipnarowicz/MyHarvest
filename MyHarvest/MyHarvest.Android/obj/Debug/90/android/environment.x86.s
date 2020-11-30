df89e32d04b9f7c194b2e00e9ad7686ce578251e
marcinhipnarowicz <58361587+marcinhipnarowicz@users.noreply.github.com>
2020-11-30 14:50:02 +0100

    zmiany nWyglądu

--- MyHarvest/MyHarvest.Android/obj/Debug/90/android/environment.x86.s
+++ MyHarvest/MyHarvest.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"5099f555-6d9a-48a6-a79f-ba0a619fdbd4"
+	.asciz	"c315ce48-5ab5-4311-a2d6-aa3d246e3be3"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
